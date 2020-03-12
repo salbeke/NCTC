394ca1407c7435722a412669c2c8612c7ca618f8
Trng Laptops <training@training.nctc>
2020-03-11 21:44:38 -0400

    Create new spatial demo script

---
+++ R/SpatialDemo.R
@@ -0,0 +1,6 @@
+# This is for playing with spatial data
+require(sf)
+require(raster)
+require(tidyverse)
+require(mapview)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NCTC
 Class Notes and Demos
+
+## My class is confused
